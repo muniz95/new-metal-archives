fb692b3eba138815855ca6a6805a4b2eccdc943d
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2016-06-07 01:22:23 -0300

    Página 259

--- config/karma.config.js
+++ config/karma.config.js
@@ -15,6 +15,14 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      '../public/vendor/angular/angular.js',
+      '../public/vendor/angular-mocks/angular-mocks.js',
+      '../public/vendor/angular-resource/angular-resource.js',
+      '../public/vendor/angular-route/angular-route.js',
+      '../public/js/main.js',
+      '../public/js/controllers/**/*.js',
+      '../public/js/services/**/*.js',
+      '../test/spec/**/*Spec.js'
     ],
 
 
