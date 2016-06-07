3f5fd2870ce67264b635a22aa65289d4ead77280
Rodrigo Muniz <muniz95@bsd.com.br>
2016-06-07 18:00:40 +0000

    cssmin não funciona

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,50 @@
-module.exports = function(grunt){
-    grunt.initConfig({
-        copy: {
-            project: {
-                expand: true,
-                cwd: '.',
-                src: ['**','!Gruntfile.js','!package.json','!bower.json'],
-                dest: 'dist'
-            }
-        },
-        clean: {
-            dist: {
-                src: 'dist'
-            }
-        },
-        usemin: {
-            html: 'dist/app/views/**/*.ejs'
-        },
-        useminPrepare: {
-            options: {
-                root: 'dist/public',
-                dest: 'dist/public',
-            },
-            html: 'dist/app/views/**/*.ejs'
-        },
-        ngAnnotate: {
-            scripts: {
-                expand: true,
-                src: ['dist/public/js/**/*.js']
-            }
-        }
-    });
+module.exports = function(grunt) {
+  grunt.initConfig({
+    copy: {
+      project: {
+        expand: true,
+        cwd: '.', 
+        src: ['**', '!Gruntfile.js', '!package.json', 
+                   '!public/bower.json'],
+        dest: 'dist'
+      }
+    }, 
     
-    grunt.registerTask('default', ['dist','minify']);
-    grunt.registerTask('dist', ['clean','copy']);
-    grunt.registerTask('minify', ['useminPrepare','ngAnnotate','concat','uglify','cssmin','usemin']);
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-ng-annotate');
-}
+    clean: {
+      dist: {
+        src: 'dist'
+      }
+    },
+
+    usemin : {
+      html: 'dist/app/views/**/*.ejs'
+    },
+
+    useminPrepare: {
+      options: {
+        root: 'dist/public',
+        dest: 'dist/public'
+      },
+      html: 'dist/app/views/**/*.ejs'
+    }, 
+    ngAnnotate: {
+      scripts: {
+        expand: true, 
+        src: ['dist/public/js/**/*.js']
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['dist', 'minify']);
+  grunt.registerTask('dist', ['clean', 'copy']);
+  grunt.registerTask('minify', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+};
