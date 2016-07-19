fb33ce989998df3cac5c5a9ac7c9ed5bbf88c9dc
Rodrigo Muniz <muniz95@bsd.com.br>
2016-07-19 15:45:51 +0000

    Index sem login

--- public/js/main.js
+++ public/js/main.js
@@ -21,5 +21,5 @@ angular.module('new-metal-archives', ['ngRoute','ngResource'])
       templateUrl: 'partials/auth.html'
     });
 
-    $routeProvider.otherwise({redirectTo: '/bands'});
+    $routeProvider.otherwise({redirectTo: '/'});
 });
