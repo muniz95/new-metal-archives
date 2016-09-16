78d735edd6d1fb7d2bbbfafcdea73bf3594bd3f5
Rodrigo Muniz <muniz95@bsd.com.br>
2016-09-16 12:50:32 +0000

    removidas referências do passport e helmet

--- config/express.js
+++ config/express.js
@@ -2,9 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
-var passport = require("passport");
+// var passport = require("passport");
 var load = require('express-load');
-var helmet = require('helmet');
+// var helmet = require('helmet');
 
 module.exports = function(){
   var app = express();
@@ -26,13 +26,13 @@ module.exports = function(){
     resave: true,
     saveUninitalized: true
   }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(helmet());
-  app.disable('x-powered-by');
-  app.use(helmet.xframe());
-  app.use(helmet.xssFilter());
-  app.use(helmet.nosniff());
+  // app.use(passport.initialize());
+  // app.use(passport.session());
+  // app.use(helmet());
+  // app.disable('x-powered-by');
+  // app.use(helmet.xframe());
+  // app.use(helmet.xssFilter());
+  // app.use(helmet.nosniff());
 
   load('models', {cwd: 'app'})
     .then('controllers')
