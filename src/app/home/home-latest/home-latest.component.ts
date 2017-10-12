7a46e7439587ba1be1f869df634bfb5ebcb5294b
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2017-10-12 01:08:24 -0300

    Home page partially done

---
+++ src/app/home/home-latest/home-latest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-latest',
+  templateUrl: './home-latest.component.html',
+  styleUrls: ['./home-latest.component.css']
+})
+export class HomeLatestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HomeNewsComponent } from './home-news/home-news.component';
+import { HomeLatestComponent } from './home-latest/home-latest.component';
+import { TabsModule } from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TabsModule.forRoot()
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, HomeNewsComponent, HomeLatestComponent]
 })
 export class HomeModule { }
