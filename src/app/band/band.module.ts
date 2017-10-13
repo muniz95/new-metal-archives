b27b6fbcbb0eb9f255fa7170fbd5c89c25f64af2
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2017-10-12 21:47:26 -0300

    Page of bands by genre added

--- src/app/band/band.module.ts
+++ src/app/band/band.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BandsAlphabeticalComponent } from './bands-alphabetical/bands-alphabetical.component';
 import { BandsCountryComponent } from './bands-country/bands-country.component';
+import { BandsGenreComponent } from './bands-genre/bands-genre.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BandsAlphabeticalComponent, BandsCountryComponent]
+  declarations: [BandsAlphabeticalComponent, BandsCountryComponent, BandsGenreComponent]
 })
 export class BandModule { }
--- src/app/band/band.routing.ts
+++ src/app/band/band.routing.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BandsAlphabeticalComponent } from './bands-alphabetical/bands-alphabetical.component'
 import { BandsCountryComponent } from 'app/band/bands-country/bands-country.component';
+import { BandsGenreComponent } from 'app/band/bands-genre/bands-genre.component';
 
 const bandRoutes: Routes = [
   { path: 'bands/alphabetical', pathMatch: 'full', component: BandsAlphabeticalComponent },
-  { path: 'bands/country', pathMatch: 'full', component: BandsCountryComponent }
+  { path: 'bands/country', pathMatch: 'full', component: BandsCountryComponent },
+  { path: 'bands/genre', pathMatch: 'full', component: BandsGenreComponent }
 ];
 
 export const BandRouting: ModuleWithProviders = RouterModule.forRoot(bandRoutes);
