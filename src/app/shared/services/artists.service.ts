8d65970092bf999867a6ef1822fbc75ec3c1dfd1
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-18 00:58:00 -0300

    Migration to ng6 successful

--- src/app/shared/services/artists.service.ts
+++ src/app/shared/services/artists.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ArtistsService {
@@ -8,8 +8,7 @@ export class ArtistsService {
   constructor(private http: Http) { }
 
   getAllBands() {
-    return this.http.get('/api/artists')
-      .map(res => res.json());
+    return this.http.get('/api/artists');
   }
 
 }
--- src/app/shared/services/bands.service.ts
+++ src/app/shared/services/bands.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class BandsService {
@@ -8,8 +8,7 @@ export class BandsService {
   constructor(private http: Http) { }
 
   getAllBands() {
-    return this.http.get('/api/bands')
-      .map(res => res.json());
+    return this.http.get('/api/bands');
   }
 
 }
