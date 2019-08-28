410584e923f5d6296eb12db4fba72f4f2f10a5d8
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-08-28 13:11:10 -0300

    Reports page done

---
+++ src/components/reports/index.js
@@ -0,0 +1,9 @@
+import Unassigned from './Unassigned'
+import Assigned from './Assigned'
+import Resolved from './Resolved'
+
+export default {
+  Unassigned,
+  Assigned,
+  Resolved
+}
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,10 +1,49 @@
 <template>
-  <div>Reports</div>
+  <div>
+    <h1>Rules & Guidelines</h1>
+    <p>
+      Want to help? Below is a list of all the outstanding reports.
+      If you have the access, go ahead and fix them.
+      Check out these tips and guidelines. And thanks!
+    </p>
+    <p>
+      You can also help by looking at all this other stuff to work on.
+    </p>
+    <div>
+      <div>
+        <button v-on:click="showTab('unassigned', 'update')">Unassigned</button>
+        <button v-on:click="showTab('assigned', 'update')">Assigned</button>
+        <button v-on:click="showTab('resolved', 'update')">Resolved</button>
+      </div>
+
+      <div id="unassigned" class="update">
+        <h2>Unassigned</h2>
+        <Unassigned />
+      </div>
+
+      <div id="assigned" class="update" style="display:none">
+        <h2>Assigned</h2>
+        <Assigned />
+      </div>
+
+      <div id="resolved" class="update" style="display:none">
+        <h2>Resolved</h2>
+        <Resolved />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ReportsComponents from '@/components/reports'
 export default {
-
+  components: ReportsComponents,
+  methods: {
+    showTab (city, type) {
+      [...document.getElementsByClassName(type)].forEach(el => { el.style.display = 'none' })
+      document.getElementById(city).style.display = 'block'
+    }
+  }
 }
 </script>
 
