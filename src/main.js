4b0bc12abd94103a5d225eaaf1c8029bd294dba5
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2020-05-19 23:17:37 -0300

    font-awesome added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import './index.scss'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+Vue.component('fai', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
